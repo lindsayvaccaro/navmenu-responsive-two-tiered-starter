95a33d1250a5af9a77d21b756aac63eee8d363c0
lindsayvaccaro <lvaccaro@mail.endicott.edu>
2026-04-16 13:41:56 -0400

    Website Set-Up

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,32 @@
 
 //  Yet another way to add the current year to the footer
 document.querySelector("#copyright").textContent = new Date().getFullYear();
+
+// mobile menu //
+const navigationMenu = document.getElementById("navmenu");
+const navButton = document.getElementById("toggle-menu ");
+
+function toggleMenu() {
+    let isVisible = navigationMenu.getAttribute("data-visible");
+    if(isVisible === "false") {
+        if(navigationMenu.classList.contains("active")) {
+            navigationMenu.setAttribute("data-visible","true");
+            navButton.setAttribute("data-visible", "true");
+        }
+        else {
+            dataDisplay();
+            setTimeout(toggleMenu, 200);
+        }
+    }
+    else {
+        navigationMenu.setAttribute("data-visible", "false");
+        navButton.setAttribute("data-visible", "false");
+        setTimeout(dataDisplay, 200);
+    }
+
+    function dataDisplay() {
+        navigationMenu.classList.toggle("active");
+    }
+}
+
+navButton.addEventListener("click", toggleMenu)
